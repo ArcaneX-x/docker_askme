d1e3f4df1fbfeccbc21a75f2eff66cdc949cc1ee
axelrod <fasolshik@yahoo.com>
2023-02-05 15:03:47 +0600

    Docker update

---
+++ db/migrate/20221224145650_create_hashtag_questions.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateHashtagQuestions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :hashtag_questions do |t|
+      t.belongs_to :question, null: false, foreign_key: true
+      t.belongs_to :hashtag, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
